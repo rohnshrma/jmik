a730ce54e2345febc7530a2fb5772bb173523587
rohnshrma <rohn11shrma@gmail.com>
2024-03-18 23:34:19 +0530

    async await

--- async/weather.js
+++ async/weather.js
@@ -91,43 +91,43 @@
 
 // 
 
-function yourPromise(){
+function yourPromise() {
     var myName = prompt("Enter your name : ")
-    return new Promise(function(resolve,reject){
-    
-        setTimeout(()=>{
-            if(myName.length >= 3){
+    return new Promise(function (resolve, reject) {
+
+        setTimeout(() => {
+            if (myName.length >= 3) {
                 resolve(`${myName} is welcomed.`)
-            }else{
+            } else {
                 reject(`${myName} is not an actual name!`)
             }
-        },5000)
-    
+        }, 5000)
+
     })
 }
 
-yourPromise()
-.then(data=>{
-    console.log(data);
-    yourPromise()
-    .then(data=>{
-        console.log(data);
-        yourPromise()
-        .then(data=>{
-            console.log(data);
-            yourPromise()
-            .then(data=>{
-                console.log(data);
-            })
-        
-        })
-    
-    })
+// yourPromise()
+// .then(data=>{
+//     console.log(data);
+//     yourPromise()
+//     .then(data=>{
+//         console.log(data);
+//         yourPromise()
+//         .then(data=>{
+//             console.log(data);
+//             yourPromise()
+//             .then(data=>{
+//                 console.log(data);
+//             })
 
-})
-.catch(err=>{
-    console.log(err);
-})
+//         })
+
+//     })
+
+// })
+// .catch(err=>{
+//     console.log(err);
+// })
 
 
 
@@ -135,3 +135,59 @@ yourPromise()
 
 // create a function called getWeatherInfo which takes the city name as parameter
 // and returns a "Promise" shows the weather data of that city if resolved else shows "data not found". 
+
+
+// yourPromise()
+// .then(data=>{
+//     console.log(data);
+
+// })
+// .catch(err=>{
+//     console.log(err);
+// })
+
+
+
+
+
+
+
+var fetchData = async (city) => {
+    try {
+        var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3ed1a8944ae36bde087adc8f67d0f04a&units=metric`)
+        console.log("response => ", response);
+        var data = await response.json()
+        console.log("final data =>",data);
+    } catch (err) {
+        console.log("Errors => ", err);
+    }
+
+}
+
+fetchData("dublin")
+
+
+
+
+
+
+
+
+
+// async function readData() {
+
+//     try {
+//         // run an async code
+//         var response = await yourPromise()
+//         console.log("success => ", response)
+//     }
+//     catch (err) {
+//         console.log("err => ", err);
+//     }
+
+// }
+
+
+// readData()
+
+
